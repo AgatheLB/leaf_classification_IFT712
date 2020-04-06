d50c081e59ef00db2cee2054e84b263c12f29f9e
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2020-04-06 12:37:54 -0400

    dataloader class

--- prog/main.py
+++ prog/main.py
@@ -1,16 +1,30 @@
 import sklearn
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import argparse
+from data_handler import LCDataset
+import pandas as pd
+
 
 def argument_parser():
-    parser = argparse.ArgumentParser(description='Classification de feuille d arbre utilisant 6 methode de classification differentes.')
-    parser.add_argument('--method', type=str,
-                         description='Permet d,utiliser la methode specifie.', choices=['MLP','regression','SVM','randomforest','adaboost'])
+    parser = argparse.ArgumentParser(description='Classification de feuille d arbre utilisant 6 methode '
+                                                 'de classification differentes.')
+    parser.add_argument('-m', '--method', type=str,
+                        help='Permet d,utiliser la methode specifie.',
+                        choices=['MLP','regression','SVM','randomforest','adaboost', 'linear_discriminant_analysis'],
+                        required=True)
+    parser.add_argument('-i', '--train_path', type=str, help='Chemin du jeu de données d\'entrainement', required=True)
+    parser.add_argument('-t', '--test_path', type=str, help='Chemin du jeu de données de test', required=True)
+
+    parser = parser.parse_args()
+
     return parser
 
 
-if __name__ == "__main__":
+def main():
+
     args = argument_parser()
 
+    dataset = LCDataset(args.train_path, args.test_path)
     method = args.method
 
     if method == 'MLP':
@@ -23,7 +37,14 @@ if __name__ == "__main__":
         pass
     elif method == 'adaboost':
         pass
-    elif method == '':
-        pass
+    elif method == 'linear_discriminant_analysis':
+        linear_discriminant_analysis(dataset)
     else:
         raise Exception('not a valid method')
+
+
+def linear_discriminant_analysis():
+    pass
+
+if __name__ == "__main__":
+    main()
