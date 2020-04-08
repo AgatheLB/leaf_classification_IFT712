77c957868cfccf272375846d5a7179b87942a442
Philippe Spino <philippe.spino@usherbrooke.ca>
2020-04-08 14:45:51 -0400

    created regression class with Ridge regression change the way we run the classifier. now we create the classifier object and then we parse a list of classifiers and execute them

--- prog/main.py
+++ prog/main.py
@@ -7,22 +7,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from zipfile import ZipFile
-<<<<<<< HEAD
 from classifier.NB import NB
 from classifier.LDA import LDA
 from classifier.RF import RF
 from classifier.MLP import MLP
 from classifier.SVM import SVM
 from classifier.KNN import KNN
+from classifier.Regression import Regression
 from sklearn.preprocessing import LabelEncoder
-
-
-=======
-from classifier.MLP import MLP
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.metrics import accuracy_score
->>>>>>> completed MLP model analysis and classification
 
 def argument_parser():
     parser = argparse.ArgumentParser(description='Classification de feuille d arbre utilisant 6 methode de classification differentes.')
@@ -59,9 +51,6 @@ def createDataSets():
     train = train.drop(['species','id'], axis=1)
     test = test.drop(['id'], axis=1)
 
-    train = train.to_numpy()
-    test = test.to_numpy()
-
     return train, labels, test, test_ids, classes
 
 
@@ -70,65 +59,40 @@ if __name__ == '__main__':
     method = args.method
 
     train, labels, test, test_ids, classes = createDataSets()
-<<<<<<< HEAD
-=======
+    classifiers = []
 
     if method == 'MLP':
-        classifier = MLP(train, test, labels, test_ids, classes)
-        
-        hlayers = [ (10,),
-                    (10,10),
-                    (100,),
-                    (100,100)
-        ]
-        lr = [  1e-3,
-                2e-3,
-                3e-3,
-                1e-4,
-                2e-4,
-                5e-5
-        ]
-
-        mlp, hyperParams = classifier.hyperparamSearch(hiddens=hlayers,learning_rates=lr)
-        print('meilleure hyper parametre trouver: {}'.format(hyperParams))
-        test_pred_prob = mlp.predict_proba(test)
-        
-        rndIdx = np.random.randint(0, len(test_pred_prob))
-        top10idx = test_pred_prob[rndIdx].argsort()[-10:][::-1]
-        top10Classes = classes[top10idx]
-        test = test_pred_prob[rndIdx][top10idx]
-        plt.bar(top10Classes,test)
-        plt.xticks(rotation=-45)
-        plt.show()
->>>>>>> completed MLP model analysis and classification
-
-    if method == 'MLP':
-        classifier = MLP(train, labels, test, test_ids, classes)
+        clf = MLP(train, labels, test, test_ids, classes)
+        classifiers.append(clf)
     elif method == 'regression':
-        pass
+        clf = Regression(train, labels, test, test_ids, classes)
+        classifiers.append(clf)
     elif method == 'SVM':
-        classifier = SVM(train, labels, test, test_ids, classes)
+        clf = SVM(train, labels, test, test_ids, classes)
+        classifiers.append(clf)
     elif method == 'randomforest':
-        classifier = RF(train, labels, test, test_ids, classes)
+        clf = RF(train, labels, test, test_ids, classes)
+        classifiers.append(clf)
     elif method == 'adaboost':
         pass
     elif method == 'KNN':
-        classifier = KNN(train, labels, test, test_ids, classes)
+        clf = KNN(train, labels, test, test_ids, classes)
+        classifiers.append(clf)
     elif method == 'naive_bayes':
-        classifier = NB(train, labels, test, test_ids, classes)
+        clf = NB(train, labels, test, test_ids, classes)
+        classifiers.append(clf)
     elif method == 'linear_discriminant_analysis':
-        classifier = LDA(train, labels, test, test_ids, classes)
+        clf = LDA(train, labels, test, test_ids, classes)
+        classifiers.append(clf)
     elif method == 'all':
-        classifiers = [MLP, SVM, RF, KNN, NB, LDA]
-        for clf in classifiers:
+        clfs = [MLP, SVM, RF, KNN, NB, LDA]
+        for clf in clfs:
             classifier = clf(train, labels, test, test_ids, classes)
-            classifier.search_hyperparameters()
-            classifier.train()
-            classifier.display_accuracies()
+            classifiers.append(classifiers)
     else:
         raise Exception('not a valid method')
 
-    if method != "all":
-        classifier.search_hyperparameters()
-        classifier.train()
-        classifier.display_accuracies()
+    for c in classifiers:
+        c.search_hyperparameters()
+        c.train()
+        c.display_accuracies()
